4bc3517778109f27ebc2881850e49806c1364b2e
John Cruz <jbcruz0209@gmail.com>
2019-11-21 07:19:41 -0800

    audit message no issues

--- server.js
+++ server.js
@@ -1,16 +1,83 @@
-const express = require("express");
-const morgon = require("morgan");
+require('dotenv').config();
+const express = require('express');
+const morgon = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
-app.use(morgon("dev"));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res) => {
-  res.send("Hello, Cow");
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  console.log('validate bearer token middleware'); // testing app.use middleware => use is middleware
+  // move to the next middleware
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  //move to the next middleware
+  next();
 });
 
-const PORT = 8000;
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`
+];
 
-app.listen(PORT, () => {
-  console.log("Server listening at http://localhost:${PORT}");
+app.get('/types', function handleGetTypes(req, res) {
+  res.json(validTypes);
 });
+
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+
+  //filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      //case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+  //filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(poke => pokemon.type.includes(req.query.type));
+  }
+
+  app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' } };
+    } else {
+      response = { error };
+    }
+    res.status(500).json(response);
+  });
+
+  res.json(response);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
